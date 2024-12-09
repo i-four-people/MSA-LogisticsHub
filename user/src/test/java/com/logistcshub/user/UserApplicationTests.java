6c5a7ad7b041ecd327928d7728518c7ac45d08dd
afoisl <ruaql0810@gmail.com>
2024-12-09 23:56:53 +0900

    feat(user): add user

---
+++ user/src/test/java/com/logistcshub/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logistcshub.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
