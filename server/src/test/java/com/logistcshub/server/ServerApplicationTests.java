153684633c89118b3b061cb9d38f0202951c8002
younggeun <chldudrms94@naver.com>
2024-12-09 16:29:42 +0900

    chore: add eureka server

---
+++ server/src/test/java/com/logistcshub/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logistcshub.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
