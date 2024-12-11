230b34e16c02bec6cfe53599a8fa121dfb678d30
younggeun <chldudrms94@naver.com>
2024-12-12 01:34:18 +0900

    feat: hub entity 추가

---
+++ hub/src/main/java/com/logistcshub/hub/hub/domain/mode/Hub.java
@@ -0,0 +1,51 @@
+package com.logistcshub.hub.hub.domain.mode;
+
+import com.logistcshub.hub.area.domain.model.Area;
+import com.logistcshub.hub.common.domain.model.BaseEntity;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+
+@Entity
+@Table(name = "p_hubs")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Hub extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Comment("허브 아이디")
+    private UUID id;
+
+    @Column
+    private String name;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private double lat;
+
+    @Column(nullable = false)
+    private double lng;
+
+    @JoinColumn
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Area area;
+
+
+}
