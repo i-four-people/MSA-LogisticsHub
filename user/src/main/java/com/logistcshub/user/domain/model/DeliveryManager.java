057690f3b17809161c6ab18456464f6674cca043
afoisl <ruaql0810@gmail.com>
2024-12-11 23:22:49 +0900

    Feat: 배송담당자 엔티티 추가

---
+++ user/src/main/java/com/logistcshub/user/domain/model/DeliveryManager.java
@@ -0,0 +1,41 @@
+package com.logistcshub.user.domain.model;
+
+import com.logistcshub.user.infrastructure.common.AuditEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "p_delivery_managers")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder(access = AccessLevel.PRIVATE)
+public class DeliveryManager extends AuditEntity {
+
+    @Id
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column
+    private UUID hubId;
+
+    @Column(nullable = false, length = 50)
+    @Enumerated(EnumType.STRING)
+    private DeliveryManagerType deliveryPersonType;
+
+    // 배송 담당자 등록
+    public static DeliveryManager create(Long userId, UUID hubId, DeliveryManagerType deliveryPersonType) {
+        return DeliveryManager.builder()
+                .id(userId)
+                .userId(userId)
+                .hubId(hubId)
+                .deliveryPersonType(deliveryPersonType)
+                .build();
+    }
+}
