f47648f5ebd6697ef437b3af6d24243568960bb9
afoisl <ruaql0810@gmail.com>
2024-12-11 12:47:56 +0900

    Fest: 내 정보 조회 기능 추가

--- user/src/main/java/com/logistcshub/user/application/service/UserService.java
+++ user/src/main/java/com/logistcshub/user/application/service/UserService.java
@@ -1,6 +1,9 @@
 package com.logistcshub.user.application.service;
 
+import com.logistcshub.user.application.dtos.MyInfoDto;
+import com.logistcshub.user.domain.model.User;
 import com.logistcshub.user.domain.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +12,11 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+
+    public MyInfoDto getMyInfo(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("등록하지 않은 유저입니다."));
+
+        return MyInfoDto.of(user);
+    }
 }
--- user/src/main/java/com/logistcshub/user/presentation/controller/UserController.java
+++ user/src/main/java/com/logistcshub/user/presentation/controller/UserController.java
@@ -1,15 +1,29 @@
 package com.logistcshub.user.presentation.controller;
 
+import com.logistcshub.user.application.dtos.MyInfoDto;
 import com.logistcshub.user.application.service.UserService;
+import com.logistcshub.user.infrastructure.common.ApiResponse;
+import com.logistcshub.user.infrastructure.common.MessageType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
+
+    @GetMapping("/user/{id}")
+    public ApiResponse<MyInfoDto> getMyInfo(@PathVariable Long id) {
+        return ApiResponse.<MyInfoDto>builder()
+                .messageType(MessageType.RETRIEVE)
+                .data(userService.getMyInfo(id))
+                .build();
+    }
+
 }
