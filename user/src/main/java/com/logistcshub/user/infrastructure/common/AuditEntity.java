780a90ec56d18059540fc12f6e27ce97a6c19936
afoisl <ruaql0810@gmail.com>
2024-12-11 23:06:06 +0900

    Feat: AuditEntity 추가

---
+++ user/src/main/java/com/logistcshub/user/infrastructure/common/AuditEntity.java
@@ -0,0 +1,51 @@
+package com.logistcshub.user.infrastructure.common;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+public class AuditEntity {
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @CreatedBy
+    @Column(name = "created_by", updatable = false)
+    private String createdBy;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @LastModifiedBy
+    @Column(name = "updated_by")
+    private String updatedBy;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    @Column(name = "deleted_by")
+    private String deletedBy;
+
+    @Column(name = "is_delete")
+    private Boolean isDelete = false;
+
+    // 논리적 삭제 메서드
+    public void delete(String email) {
+        deletedAt = LocalDateTime.now();
+        deletedBy = email;
+        isDelete = true;
+    }
+
+}
