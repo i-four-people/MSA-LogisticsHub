1a4cb89b70bcd09fe1b649921d7966fd51372f23
afoisl <ruaql0810@gmail.com>
2024-12-12 16:17:26 +0900

    Chore: Application 어노테이션 추가

--- user/src/main/java/com/logistcshub/user/UserApplication.java
+++ user/src/main/java/com/logistcshub/user/UserApplication.java
@@ -2,8 +2,14 @@ package com.logistcshub.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class UserApplication {
 
 	public static void main(String[] args) {
