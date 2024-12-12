4f4cd5313f4df0e017d18cf8226bb67cdc8dad57
afoisl <ruaql0810@gmail.com>
2024-12-13 01:14:29 +0900

    Chore: HubManager 엔드포인트 변경

--- user/src/main/java/com/logistcshub/user/presentation/controller/HubManagerController.java
+++ user/src/main/java/com/logistcshub/user/presentation/controller/HubManagerController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/hub-manager")
+@RequestMapping("/api/hub-managers")
 @RequiredArgsConstructor
 public class HubManagerController {
 
