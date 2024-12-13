150bc1fb16d5f8d2bc43cff74975ace425985b98
laira2 <laira2@gachon.ac.kr>
2024-12-13 14:07:18 +0900

    Refactor: - company 조회 PagedModel로 변경 - queryDsl을 사용한 검색기능 리팩터링

--- company/src/main/java/com/logistcshub/company/domain/repository/CompanyRepository.java
+++ company/src/main/java/com/logistcshub/company/domain/repository/CompanyRepository.java
@@ -2,10 +2,13 @@ package com.logistcshub.company.domain.repository;
 
 import com.logistcshub.company.domain.model.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface CompanyRepository extends JpaRepository<Company, UUID>, CompanyRepositoryCustom {
+public interface CompanyRepository extends JpaRepository<Company, UUID>,
+        CompanyRepositoryCustom ,
+        QuerydslPredicateExecutor<Company> {
 }
--- company/src/main/java/com/logistcshub/company/domain/repository/CompanyRepositoryCustom.java
+++ company/src/main/java/com/logistcshub/company/domain/repository/CompanyRepositoryCustom.java
@@ -7,13 +7,5 @@ import org.springframework.data.domain.Pageable;
 import java.util.UUID;
 
 public interface CompanyRepositoryCustom {
-    Page<Company> getCompanies(UUID id,
-                               String name,
-                               String companyType,
-                               String address,
-                               String contact,
-                               String sortBy,
-                               boolean isAsc,
-                               Pageable pageable);
 
 }
